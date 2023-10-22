b3f5c6f273595f5eb4799103f4271f54867c6865
Mohd Sarfaraz <onlinemedsdirect@gmail.com>
2023-10-22 18:06:49 +0530

    first commit

---
+++ backend/data.js
@@ -0,0 +1,104 @@
+module.exports= {
+    products:[
+        {
+            _id:'xraymachine',
+            name:'XRAY MACHINE',
+            category:'medical',
+            image:'images/product-1.jpg',
+            price: 0,
+            detail: '',
+            description:"",
+            countInStock: 4
+        },
+        {
+            _id:'drwpf',
+            name:'DIGITAL RADIOGRAPHY WITH FLAT PANEL DETECTOR',
+            category:'medical',
+            image:'images/product-2.jpg',
+            price: 0,
+            detail: '',
+            description:" ",
+            countInStock: 4
+        },
+        {
+            _id:'carmmachine',
+            name:'c arm machine',
+            category:'medical',
+            image:'images/product-3.jpg',
+            price: 0,
+            detail: '',
+            description:"",
+            countInStock: 4
+        },
+        {
+            _id:'idf',
+            name:'ILR DEEP FREEZER',
+            category:'medical',
+            image:'images/product-4.jpg',
+            price: 0,
+            detail: '',
+            description:"",
+            countInStock: 4
+        },
+        {
+            _id:'ventilator',
+            name:'VENTILATIOR',
+            category:'medical',
+            image:'images/product-5.jpg',
+            price: 0,
+            detail: '',
+            description:"",
+            countInStock: 4
+        },
+        // {
+        //     _id:'phone',
+        //     name:'Auqa Phone',
+        //     category:'Phone',
+        //     image:'images/product-6.jpg',
+        //     price: 600,
+        //     detail: '$600',
+        //    description:"Welcome to our collection of cutting-edge phones, where innovation meets style to provide you with the ultimate communication experience. Designed to keep you connected, entertained, and productive, these phones are packed with advanced features that cater to your every need.",
+        //     countInStock: 4
+        // },
+        // {
+        //     _id:'coloredpencils',
+        //     name:'Colored pencils',
+        //     category:'Stationary',
+        //     image:'images/product-7.jpg',
+        //     price: 10,
+        //     detail: '$10 for pack of colored pencils',
+        //     description:"Welcome to our collection of premium colored pencils, where vibrant hues and superior quality come together to ignite your artistic potential. Whether you're a professional artist, an aspiring creative, or someone who enjoys the meditative art of coloring, our colored pencils will elevate your artistic endeavors to new heights.",
+        //     countInStock: 4
+        // },
+        // {
+        //     _id:'ladiesheels',
+        //     name:'Heels',
+        //     category:'Heels',
+        //     image:'images/product-8.jpg',
+        //     price: 350,
+        //     detail: '$350 for heels',
+        //     description:"Welcome to our exquisite collection of heels, where elegance meets sophistication to redefine your footwear game. Whether you're attending a special occasion, dressing up for a night out, or adding a touch of glamour to your everyday wear, our heels are designed to make you feel confident and fabulous with every step you take.",
+        //     countInStock: 4
+        // },
+        // {
+        //     _id:'tap',
+        //     name:'Stylish-sink-tap',
+        //     category:'Tap',
+        //     image:'images/product-9.jpg',
+        //     price: 50,
+        //     detail: '$50 for tap',
+        //     description:"Introducing our exquisite collection of stylish sink taps, where sophistication and practicality converge to elevate the aesthetics of your kitchen or bathroom. Carefully curated with an eye for detail, these taps are designed to be the centerpiece of your space, seamlessly blending beauty and functionality.",
+        //     countInStock: 4
+        // },
+        // {
+        //     _id:'brush',
+        //     name:'Original Detangling Brush',
+        //     category:'Brush',
+        //     image:'images/product-10.jpg',
+        //     price: 45,
+        //     detail: '$45 for Brush',
+        //     description:" Detangling Brush for Curly Hair - Tangle-Free Brush for Straight, Thick, & Wavy Hair",
+        //     countInStock: 4
+        // },
+    ]
+};
